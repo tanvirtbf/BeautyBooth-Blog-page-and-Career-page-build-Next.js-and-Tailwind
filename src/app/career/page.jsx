49f379c47d5ceb717934e076675195da371c63a7
tanvirtbf <tanver018765@gmail.com>
2024-05-20 15:44:19 +0600

    career page banner complete

--- src/app/career/page.jsx
+++ src/app/career/page.jsx
@@ -1,9 +1,17 @@
+import Image from 'next/image'
 import React from 'react'
+import Banner from '../../../public/Banner.png'
 
 const page = () => {
   return (
-    <div>
-      career page
+    <div className='container'>
+      <div>
+        <Image src={Banner} alt='banner' priority/>
+      </div>
+      <div className='mt-8'>
+        <h1 className=''>Career at Beautybooth</h1>
+      </div>
+      <div></div>
     </div>
   )
 }
