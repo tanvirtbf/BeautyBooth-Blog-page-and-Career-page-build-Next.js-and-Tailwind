7d57a5ef29d2029f5fb1d93378adca7e5ebf258b
tanvirtbf <tanver018765@gmail.com>
2024-05-20 20:37:19 +0600

    data separate

---
+++ src/app/Data.js
@@ -0,0 +1,8 @@
+export const roleArr = [
+  {id: Date.now(), heading:"Junior UX Designer", remoteOrOffice: false, state:"Junior", positions:2, btnState:true},
+  {id: Date.now(), heading:"Marketing Specialist", remoteOrOffice: true, state:"2-5 Years", positions:2, btnState:true},
+  {id: Date.now(), heading:"Backend Developer", remoteOrOffice: true, state:"Junior", positions:2, btnState:true},
+  {id: Date.now(), heading:"Content Writer", remoteOrOffice: false, state:"Mid-level", positions:2, btnState:true},
+  {id: Date.now(), heading:"Tiktok Specialist", remoteOrOffice: true, state:"Junior", positions:2, btnState:false},
+  {id: Date.now(), heading:"Social Media Management", remoteOrOffice: false, state:"Mid-level", positions:2, btnState:false},
+]
--- src/app/career/page.jsx
+++ src/app/career/page.jsx
@@ -1,15 +1,7 @@
 import CurrentOpenRole from "@/components/CurrentOpenRole";
 import Image from "next/image";
 import Banner from "../../../public/Banner.png";
-
-const roleArr = [
-  {id: Date.now(), heading:"Junior UX Designer", remoteOrOffice: false, state:"Junior", positions:2, btnState:true},
-  {id: Date.now(), heading:"Marketing Specialist", remoteOrOffice: true, state:"2-5 Years", positions:2, btnState:true},
-  {id: Date.now(), heading:"Backend Developer", remoteOrOffice: true, state:"Junior", positions:2, btnState:true},
-  {id: Date.now(), heading:"Content Writer", remoteOrOffice: false, state:"Mid-level", positions:2, btnState:true},
-  {id: Date.now(), heading:"Tiktok Specialist", remoteOrOffice: true, state:"Junior", positions:2, btnState:false},
-  {id: Date.now(), heading:"Social Media Management", remoteOrOffice: false, state:"Mid-level", positions:2, btnState:false},
-]
+import { roleArr } from "../Data";
 
 const page = () => {
   return (
