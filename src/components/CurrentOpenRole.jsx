6ab730cb1dfcea9f221cbede8e3dd8609e039723
tanvirtbf <tanver018765@gmail.com>
2024-05-21 02:05:11 +0600

    finish btn resonsive

--- src/components/CurrentOpenRole.jsx
+++ src/components/CurrentOpenRole.jsx
@@ -13,15 +13,17 @@ const CurrentOpenRole = ({
   btnState,
   isTwoBtn,
 }) => {
-
-
   return (
     <div className="cart-shadow rounded-lg p-4 sm:p-5 bg-white grid grid-cols-12 gap-5">
       <div className="flex flex-col gap-3 col-span-12 sm:col-span-8">
-        <h3 className="font-medium text-sm sm:text-base text-[#222222]">
+        <h3
+          className={`font-medium text-sm sm:text-base text-[#222222] ${
+            isTwoBtn ? "text-base sm:text-xl" : ""
+          }`}
+        >
           {heading}
         </h3>
-        <div className={`flex ${isTwoBtn ? 'gap-6' : 'justify-between'}`}>
+        <div className={`flex ${isTwoBtn ? "gap-6" : "justify-between"}`}>
           <div className="flex gap-[6px] items-center">
             <Image src={LocationImage} alt="locationimage" />
             <span className="font-normal text-xs sm:text-sm text-[#717171]">
@@ -44,9 +46,13 @@ const CurrentOpenRole = ({
       </div>
       <div className="flex justify-start sm:justify-end items-center col-span-12 sm:col-span-4">
         {isTwoBtn ? (
-          <div className="flex gap-5">
-            <RoleCartButton btnState={btnState} id={id} isTwoBtn={true}/>
-            <RoleCartButton btnState={btnState} id={id} />
+          <div className="flex gap-5 grid-cols-12">
+            <div className="col-span-6 sm:col-span-1 ">
+              <RoleCartButton btnState={btnState} id={id} isTwoBtn={true} />
+            </div>
+            <div className="col-span-6 sm:col-span-1">
+              <RoleCartButton btnState={btnState} id={id} isTwoBtn={false} />
+            </div>
           </div>
         ) : (
           <RoleCartButton btnState={btnState} id={id} />
