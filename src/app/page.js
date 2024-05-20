e9a970b0068c266c0ba658daca3182c7a2956f6e
tanvirtbf <tanver018765@gmail.com>
2024-05-20 15:14:11 +0600

    create home button for routing

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <h1 className="text-3xl text-center text-orange-400">Hello World!</h1>
-    </>
+    <div className="w-screen h-screen flex justify-center items-center flex-col gap-4">
+      <h1 className="text-6xl text-center text-orange-400">Hello World!</h1>
+      <Link href={"/career"} className="homeBtn">Career Page</Link>
+      <Link href={"/blog"} className="homeBtn">Blog Page</Link>
+    </div>
   );
 }
---
+++ src/components/CareerBanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CareerBanner = () => {
+  return (
+    <div>
+      CareerBanner Page
+    </div>
+  )
+}
+
+export default CareerBanner
