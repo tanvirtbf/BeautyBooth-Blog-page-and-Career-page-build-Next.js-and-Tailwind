ee70ceb9d06452ed456dc1967324468d4531cd5a
tanvirtbf <tanver018765@gmail.com>
2024-05-21 17:03:58 +0600

    All Jobs Link path change

--- src/components/RoleCartButton.jsx
+++ src/components/RoleCartButton.jsx
@@ -13,7 +13,7 @@ const RoleCartButton = ({ btnState, id, isTwoBtn, className , btnText, isDisable
     )}>
       <button
         disabled={isDisable}
-        onClick={() => router.push(`/career/${id}`)}
+        onClick={() => router.push(`/career/${btnText==='All Jobs' ? '' : id}`)}
         className={cn(
           'w-[106px] sm:w-[116px] h-9 sm:h-11 text-center bg-[#00CA8D] rounded-md text-xs sm:text-sm font-medium text-white',
           className,
