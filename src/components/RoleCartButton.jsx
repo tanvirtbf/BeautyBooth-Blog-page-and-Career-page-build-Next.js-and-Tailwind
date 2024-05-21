3a235a215ab104dde1613614a9e651aab3211f44
tanvirtbf <tanver018765@gmail.com>
2024-05-21 14:38:15 +0600

    simplify RoleCurrent Button component

--- src/components/RoleCartButton.jsx
+++ src/components/RoleCartButton.jsx
@@ -4,30 +4,13 @@ import { useRouter } from "next/navigation";
 const RoleCartButton = ({ btnState, id, isTwoBtn }) => {
   const router = useRouter();
 
-  let buttonText = "";
-
-  if(btnState){
-    buttonText = "View Details"
-  } else if(!btnState){
-    buttonText = "Closed"
-  }
-  if(isTwoBtn === 'yes') {
-    buttonText = "All Jobs";
-  } else if (isTwoBtn === 'no') {
-    buttonText = "Apply Now";
-  }
-
   return (
     <>
       <button
         onClick={() => router.push(`/career/${id}`)}
-        className={`w-[106px] sm:w-[116px] h-[36px] sm:h-[44px] text-center ${
-          btnState ? "bg-[#00CA8D]" : "bg[#999999]"
-        } rounded-md text-xs sm:text-sm font-medium ${
-          isTwoBtn === 'yes' ? "bg-white text-[##5A5A5A]  border-[#DDDDDD] border-[1px] text-[#5A5A5A]" : "text-[#FFFFFF] bg-[#00CA8D]"
-        }`}
+        className={`w-[106px] sm:w-[116px] h-9 sm:h-11 text-center bg-[#00CA8D] rounded-md text-xs sm:text-sm font-medium text-white`}
       >
-        {buttonText}
+        View Details
       </button>
     </>
   );
